6dc1c08e19dad9d657e1557a2b1480bbc703372b
hyunooss <48705487+hyunooss@users.noreply.github.com>
2022-08-22 14:24:44 +0900

    first

---
+++ server/crawler.py
@@ -0,0 +1,9 @@
+
+# 뉴스 크롤링
+class News:
+    def __init__(self):
+        print("=" * 50)
+        print("[INFO] news 초기화 성공")
+         
+    def generate(self):
+        pass
---
+++ server/server.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import *
+
+
+from crawler import *
+
+app = Flask(__name__)
+
+
+News = News()
+
+
+
+# DB 연결
+engine = create_engine('mysql+pymysql://root:root@localhost/news')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+
+@app.route('/', methods=['GET', 'POST'])
+def news():
+    if request.method == 'POST':
+        
+
+    else:
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+
