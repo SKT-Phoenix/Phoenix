3a33f28ee1e79af5f24775a2e36696bb3360cc7d
Hitbee-dev <ckim.esw@gmail.com>
2022-08-23 12:20:30 +0900

    [Quest] Appbar Navigation Animation Add

--- phoenix/lib/menu/quest.dart
+++ phoenix/lib/menu/quest.dart
@@ -39,6 +39,83 @@ class _QuestState extends State<Quest> {
           )
         ],
       ),
+      body: Column(
+        children: [NavBar(), Nav_Animation(), QuestContent()],
+      ),
+    );
+  }
+
+  final List<bool> isSelected = [true, false];
+  Widget NavBar() {
+    return Container(
+      alignment: Alignment.center,
+      width: double.infinity,
+      color: Colors.white,
+      child: ToggleButtons(
+        selectedColor: Colors.black,
+        color: Colors.grey,
+        fillColor: Colors.white,
+        renderBorder: false,
+        highlightColor: Colors.white,
+        splashColor: Colors.white,
+        children: <Widget>[Nav_Content("진행 퀘스트"), Nav_Content("배지")],
+        onPressed: (int index) {
+          setState(() {
+            for (int buttonIndex = 0;
+                buttonIndex < isSelected.length;
+                buttonIndex++) {
+              if (buttonIndex == index) {
+                isSelected[buttonIndex] = true;
+              } else {
+                isSelected[buttonIndex] = false;
+              }
+            }
+          });
+        },
+        isSelected: isSelected,
+      ),
+    );
+  }
+
+  Widget Nav_Content(String name) {
+    return Container(
+        width: MediaQuery.of(context).size.width * 0.5,
+        child: Text(
+          name,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16),
+        ));
+  }
+
+  Widget Nav_Animation() {
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 1.5),
+          child: Container(
+            width: double.infinity,
+            height: 0.5,
+            color: Color.fromARGB(255, 170, 170, 170),
+          ),
+        ),
+        AnimatedAlign(
+          alignment:
+              isSelected[0] ? Alignment.centerLeft : Alignment.centerRight,
+          duration: const Duration(seconds: 1),
+          curve: Curves.fastOutSlowIn,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.5,
+            height: 2.0,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget QuestContent() {
+    return Column(
+      children: [],
     );
   }
 }
