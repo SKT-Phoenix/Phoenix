b671aa2999c3a104c1788b7317c3a5bd5e7a6aeb
Hitbee-dev <ckim.esw@gmail.com>
2022-08-24 11:38:34 +0900

    [Quest] UX/UI Clone Copy Done.

--- phoenix/lib/menu/quest.dart
+++ phoenix/lib/menu/quest.dart
@@ -52,8 +52,9 @@ class _QuestState extends State<Quest> {
               child: SizedBox(),
             ),
           ),
+          QuestTitle("매일매일 A.과 함께"),
+          QuestDailyContents(dailycontents.length),
           QuestTitle("A. 더 잘 쓰기"),
-          // QuestSubContent(1)
           QuestSubContent(subcontents.length)
         ],
       ),
@@ -130,12 +131,16 @@ class _QuestState extends State<Quest> {
   Widget QuestTitle(String string) {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
-          (context, index) => ListTile(
-                title: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                  child: Text(
-                    string,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          (context, index) => Container(
+                padding: EdgeInsets.only(top: 20),
+                child: ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+                    child: Text(
+                      string,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
               ),
@@ -151,7 +156,7 @@ class _QuestState extends State<Quest> {
     "[길안내] 'TMAP 켜줘'로 TMAP 실행하기",
     "[날씨] '날씨 알려줘'와 같이 날씨 확인하기",
   ];
-  final List<String> pointcontents = ["50", "75", "65", "35", "80", "70"];
+  final List<String> pointsubcontents = ["50", "75", "65", "35", "80", "70"];
   Widget QuestSubContent(int count) {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
@@ -174,7 +179,7 @@ class _QuestState extends State<Quest> {
                               width: 20,
                             ),
                             Text(
-                              pointcontents[index],
+                              pointsubcontents[index],
                               style: TextStyle(
                                   color: Colors.black,
                                   fontSize: 12,
@@ -202,6 +207,151 @@ class _QuestState extends State<Quest> {
                 ),
               ),
           childCount: count),
+    );
+  }
+
+  final List<String> dailycontents = [
+    "'끝말잇기 하자'와 같이 끝말잇기 대결 요청하기",
+    "'인기있는 음악 틀어줘'와 같이 음악재생 요청하기",
+    "'오늘 일정 등록해줘'같이 A. 캘린더 이용하기",
+    "'오늘의 주요 뉴스 알려줘'와 같이 이슈닷 이용하기"
+  ];
+  final List<String> pointdailycontents = ["30", "35", "30", "90"];
+  Widget QuestDailyContents(int count) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+          (context, index) => Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      width: 2,
+                      color: Custom_Utils().Colors_SKT_Blue(),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      QuestDailyTitle(),
+                      QuestDailyContent(0),
+                      QuestDailyContent(1),
+                      QuestDailyContent(2),
+                      QuestDailyContent(3),
+                    ],
+                  ),
+                ),
+              ),
+          childCount: 1),
+    );
+  }
+
+  Widget QuestDailyTitle() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 6,
+            child: Column(
+              children: [
+                Container(
+                    alignment: Alignment.centerLeft,
+                    child: QuestText("데일리 미션", Colors.black, 18)),
+                Row(
+                  children: [
+                    QuestText("2022. 8. 24. (수)까지", Colors.grey, 11),
+                    Expanded(child: SizedBox()),
+                    QuestText("0", Custom_Utils().Colors_SKT_Blue(), 11),
+                    QuestText("/${dailycontents.length}", Colors.grey, 11)
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 3,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Custom_Utils().Colors_SKT_Background(),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: Column(
+                  children: [
+                    Container(
+                        height: 35, child: Image.asset("assets/cube2.png")),
+                    QuestText("1", Colors.black, 12)
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget QuestText(String name, Color color, double size) {
+    return Text(
+      name,
+      style:
+          TextStyle(color: color, fontSize: size, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget QuestDailyContent(int index) {
+    return ListTile(
+      title: Row(children: [
+        Expanded(
+          flex: 2,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/con.png",
+                width: 20,
+              ),
+              Text(
+                pointdailycontents[index],
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              dailycontents[index],
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Expanded(
+            flex: 4,
+            child: IconButton(
+              icon: Image.asset("assets/move.png"),
+              onPressed: () {},
+            )),
+      ]),
     );
   }
 }
