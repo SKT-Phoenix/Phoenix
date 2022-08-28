4836dbafa0af2cacf8882093e7815b0fa8e888fa
JinKyeong-Seo <sjk401930@gmail.com>
2022-08-28 16:53:38 +0900

    ranking news crawling + a tag remove

--- nate_news_ranking.py
+++ nate_news_ranking.py
@@ -8,7 +8,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime, timedelta
-from pandas import DataFrame
 import pandas as pd
 import time
 from selenium.webdriver.common.by import By
@@ -39,11 +38,7 @@ def crawling_main_text(url):
     return (text.text.replace('\n','').replace('\r','').replace('<br>','').replace('\t',''), date,title)
 
 
-<<<<<<< HEAD
 part_name = ['pol', 'eco', 'soc', 'int', 'its']
-=======
-part_name = ['pol'] # , 'eco', 'soc', 'int', 'its' 정치, 경제, 사회, 세계, IT/과학
->>>>>>> fe9bd602c315e1e9d36948ce3ced8b99bb6fa8c3
 news_df=pd.DataFrame()
 
 
@@ -75,7 +70,6 @@ def news_crawling(part_name):
     
     for n in a_list:
         n_url = n.get_attribute('href')
-        print(f"분야: {part_name} / url: {n_url} / 결과: {crawling_main_text(n_url)}")
         news_dict[idx] = {'분야': part_name,
                         '타이틀' : crawling_main_text(n_url)[2],
                         '발행일자' : crawling_main_text(n_url)[1],
@@ -84,7 +78,7 @@ def news_crawling(part_name):
             
         idx += 1
 
-    part_df = DataFrame(news_dict).T
+    part_df = pd.DataFrame(news_dict).T
     return part_df
             
     
